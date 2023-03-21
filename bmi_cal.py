0b4a9464ef81861e2db9cb498f4a4fd21ad2ffb9
Tia <104528224+tiamalley@users.noreply.github.com>
2023-03-20 19:55:39 -0500

    Update bmi_cal.py

--- bmi_cal.py
+++ bmi_cal.py
@@ -8,38 +8,42 @@ def calculateBMI(heightIN, weightLB):
 
     bmi = (weight/calc)
 
-    return(bmi)
+    rounded_bmi = "{:.1f}".format(bmi)
+
+    return(rounded_bmi)
 
 # function to display the user's category
 def categoryBMI(bmi):
-    if(bmi < 18.5):
+    if(bmi < '18.5'):
         return('Underweight')
     
-    elif((bmi >= 18.5) and (bmi < 24.9)):
+    elif((bmi >= '18.5') and (bmi < '25.0')):
         return('Normal Weight')
 
-    elif((bmi >= 24.9) and (bmi < 29.9)):
+    elif((bmi >= '25.0') and (bmi < '30.0')):
         return('Overweight')
 
-    elif(bmi >= 29.9):
+    elif(bmi >= '30.0'):
         return('Obese')
 
 
+def main():
+    print('\n--- BMI Calculator ---')
+    print('Height')
 
-print('\n--- BMI Calculator ---')
-print('Height')
+    feet = float(input('Feet: '))
+    inches = float(input('Inches: '))
 
-feet = float(input('Feet: '))
-inches = float(input('Inches: '))
+    heightIN = (feet * 12) + inches
 
-heightIN = (feet * 12) + inches
+    weightLB = float(input('\nWeight(lb): '))
 
-weightLB = float(input('\nWeight(lb): '))
+    (user_bmi) = calculateBMI(heightIN, weightLB)
+    user_category = categoryBMI(user_bmi)
 
-user_bmi = calculateBMI(heightIN, weightLB)
-user_bmi_rounded = "{:.1f}".format(user_bmi)
-user_category = categoryBMI(user_bmi)
+    print('\n--- Results ---')
+    print('BMI: {}'.format(user_bmi))
+    print('Category: {}\n'.format(user_category))
 
-print('\n--- Results ---')
-print('BMI: {}'.format(user_bmi_rounded))
-print('Category: {}\n'.format(user_category))
+if __name__ == "__main__":
+    main()
